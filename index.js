3674855acd950ada3226dba8179b1d9b37498347
Jesus Prada <jesusprada177@gmail.com>
2026-06-08 13:19:52 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('T-SAFV API is running!');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
